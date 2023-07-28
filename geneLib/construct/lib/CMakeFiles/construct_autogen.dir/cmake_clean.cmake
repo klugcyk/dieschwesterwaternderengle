6dff6312902c55491bdbc5fea3aa943fb7d61593
klug <x1021122970@outlook.com>
2023-07-28 17:10:51 +0800

    delete the genelib

--- geneLib/construct/lib/CMakeFiles/construct_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/construct_autogen"
-  "construct_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/construct_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
